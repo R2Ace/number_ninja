421744515a44eca91cc991c63a35c8322a242267
Roberson Robert <robersonrobert07@gmail.com>
2024-10-30 13:01:03 -0400

    Added logic for user input

--- guessing_game.py
+++ guessing_game.py
@@ -2,5 +2,10 @@ import random
 
 #this is a game that prompts the user to guess the correct numbers as i generate random ones
 
-number = random.randrange(11)
-print(number)
+top_range = input("Type a number: ")
+
+if top_range.isdigit():
+    top_range = int(top_range)
+
+random_number = random.randrange(-5, 11)
+

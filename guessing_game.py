fc22d529b3be92dba810e4ad3621592c2ce1abae
Roberson Robert <robersonrobert07@gmail.com>
2024-10-31 10:13:56 -0700

    Addedlogic to ensure we get a number and also welcomed the user.

--- guessing_game.py
+++ guessing_game.py
@@ -2,10 +2,23 @@ import random
 
 #this is a game that prompts the user to guess the correct numbers as i generate random ones
 
-top_range = input("Type a number: ")
+print("What it do? Welcome to my basic game.")
 
+top_range = input("Please enter a number: ")
+
+#Make sure the answer we get is a digit and then convert it to an interger
 if top_range.isdigit():
     top_range = int(top_range)
 
-random_number = random.randrange(-5, 11)
+    #Making sure user enter a number greater than 0
+    if top_range <= 0:
+        print("Oopsies, Enter a number larger than 0 to proceed")
+        quit()
+else:
+    print("Oops, it's okay. Enter a number to proceed")
+
+
+#We generate random numbers based on the input range the user provided
+random_number = random.randrange(top_range)
+print(random_number)
 

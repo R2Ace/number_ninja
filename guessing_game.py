cc7f13cceea931ad8b22cef9bd32fe005e923819
Roberson Robert <robersonrobert07@gmail.com>
2024-11-01 18:47:49 -0700

    Added the logic to make the user take another guess

--- guessing_game.py
+++ guessing_game.py
@@ -4,14 +4,14 @@ import random
 
 print("What it do? Welcome to my basic game.")
 
-top_range = input("Please enter a number: ")
+user_guess = input("Please enter a number: ")
 
 #Make sure the answer we get is a digit and then convert it to an interger
-if top_range.isdigit():
-    top_range = int(top_range)
+if user_guess.isdigit():
+    user_guess = int(user_guess)
 
     #Making sure user enter a number greater than 0
-    if top_range <= 0:
+    if user_guess <= 0:
         print("Oopsies, Enter a number larger than 0 to proceed")
         quit()
 else:
@@ -19,6 +19,16 @@ else:
 
 
 #We generate random numbers based on the input range the user provided
-random_number = random.randrange(top_range)
+random_number = random.randrange(user_guess)
 print(random_number)
 
+
+#We prompt the user to make another guess
+while True:
+    user_guess = input("Make another guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Oops, it's okay. Enter a number to proceed")
+        continue
+ 
